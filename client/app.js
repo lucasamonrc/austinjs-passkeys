b0312088a8d7a8b490c8d575c4672a3b31737e06
Lucas Castro <lucasamonrc@gmail.com>
2024-07-16 10:57:21 -0500

    feature: Add markup for sign in and sign up

--- client/app.js
+++ client/app.js
@@ -1,7 +1,1 @@
-async function getMessage() {
-  const response = await fetch("/api");
-  const data = await response.text();
-  alert(data);
-}
 
-document.getElementById("btn").addEventListener("click", getMessage);
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,1 +1,8 @@
-modudle.exports = {};
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
